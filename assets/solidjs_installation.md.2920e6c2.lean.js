796ece74890124a22bf002efe2ae273de1ec9c45
Kelin2025 <Kelin2025@users.noreply.github.com>
2022-09-13 10:14:50 +0000

    deploy: 49513ce629b374ef680f06dbf9584035d758c7e5

---
+++ assets/solidjs_installation.md.2920e6c2.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as a,o as n,a as t}from"./app.34fc95c4.js";const h=JSON.parse('{"title":"SolidJS-bindings installation","description":"","frontmatter":{},"headers":[],"relativePath":"solidjs/installation.md"}'),e={name:"solidjs/installation.md"},o=t("",7),i=[o];function l(c,r,d,p,_,u){return n(),a("div",null,i)}const m=s(e,[["render",l]]);export{h as __pageData,m as default};
